93d44b917cd95071978b3048d0f859c62b795ca9
Frederick Engelns-Bauer <frederick.engelns-bauer@sap.com>
2024-10-24 14:50:06 +0200

    initial setup
    next.js and shadncn setup; added sidebar

---
+++ ramp-ui/components/ui/app-sidebar.tsx
@@ -0,0 +1,62 @@
+import {Calendar, Home, Inbox, Search, Settings} from "lucide-react"
+
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+    {
+        title: "Planner",
+        url: "#",
+        icon: Home,
+    },
+    {
+        title: "Search",
+        url: "#",
+        icon: Search,
+    },
+    {
+        title: "Calender",
+        url: "#",
+        icon: Calendar,
+    },
+    {
+        title: "Settings",
+        url: "#",
+        icon: Settings,
+    },
+]
+
+export function AppSidebar() {
+    return (
+        <Sidebar collapsible={"icon"}>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupLabel>RAMP</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {items.map((item) => (
+                                <SidebarMenuItem key={item.title}>
+                                    <SidebarMenuButton asChild>
+                                        <a href={item.url}>
+                                            <item.icon/>
+                                            <span>{item.title}</span>
+                                        </a>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+        </Sidebar>
+    )
+}
